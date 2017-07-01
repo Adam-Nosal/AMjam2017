e34eb89d13f7630813d6a321c13e761efb62c964
Adam Nosal <adam.nosal@ansharstudios.com>
2017-07-01 12:19:32 +0200

    adding scene and regenerating prefabs

---
+++ Tiled data/Maps/frame.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cp437_20x20" tilewidth="20" tileheight="20" tilecount="256" columns="16">
+ <image source="../Tilesets/cp437_20x20.png" width="320" height="320"/>
+</tileset>
