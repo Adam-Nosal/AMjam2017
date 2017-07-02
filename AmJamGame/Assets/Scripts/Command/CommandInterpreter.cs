1d753d32e4c5030428b5aa719ff669698f61af17
agatachmiel <agata.b.chmiel@gmail.com>
2017-07-02 11:02:39 +0200

    fixed bug with console enter

--- AmJamGame/Assets/Scripts/Command/CommandInterpreter.cs
+++ AmJamGame/Assets/Scripts/Command/CommandInterpreter.cs
@@ -36,7 +36,7 @@ public class CommandInterpreter : Singleton<CommandInterpreter>
 
     public void InterpretCommands()
     {
-        List<ActorCommand> commands = new List<ActorCommand>();      
+        List<ActorCommand> commands = new List<ActorCommand>();
 
 
         for (int i = 0; i < usedCommandsList.Count; i++)
@@ -44,110 +44,112 @@ public class CommandInterpreter : Singleton<CommandInterpreter>
             if (usedCommandsList[i].Length == 0 || usedCommandsList[i].Replace(" ", string.Empty).Length == 0)
             {
                 // empty
+                //  break;
             }
-
-            if (usedCommandsList[i].IndexOf('(') < 0 )
+            else
             {
-                Console2.Instance.AddFeedback(i,  TextManager.Instance.GetBacketsText());
-                return;
-            }
+                if (usedCommandsList[i].IndexOf('(') < 0)
+                {
+                    Console2.Instance.AddFeedback(i, TextManager.Instance.GetBacketsText());
+                    return;
+                }
 
-            string commandName = usedCommandsList[i].Substring(0, usedCommandsList[i].IndexOf('('));
+                string commandName = usedCommandsList[i].Substring(0, usedCommandsList[i].IndexOf('('));
 
-            if (commandsList.Find(x => x == commandName) == null) // command not found
-            {
-                Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongCommandText());
-                return;
-            }
+                if (commandsList.Find(x => x == commandName) == null) // command not found
+                {
+                    Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongCommandText());
+                    return;
+                }
 
-            switch (commandName)
-            {
-                case "mov":
-                    {
-                        int bracket = usedCommandsList[i].IndexOf('(');
-                        int comma = usedCommandsList[i].IndexOf(',');
-
-                        if(comma<0)
+                switch (commandName)
+                {
+                    case "mov":
                         {
-                            Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongNumerText());
-                            return;
-                        }
+                            int bracket = usedCommandsList[i].IndexOf('(');
+                            int comma = usedCommandsList[i].IndexOf(',');
 
-                        string param1 = usedCommandsList[i].Substring(bracket+1, comma - (bracket + 1)).Replace(" ", string.Empty);
-                        directionType dir;
-
-                        //if (param1==directionType.down.ToString() || param1 == directionType.up.ToString() || param1 == directionType.left.ToString() || param1 == directionType.right.ToString())
-                        //    dir = (directionType)Enum.Parse(typeof(directionType), param1);
-                        //else
-                        //{
-                        //    Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongFirstText());
-                        //    return;
-                        //}
-
-                        switch (param1)
-                        {
-                            case "l":
-                            case "left":
-                                dir = directionType.left;
-                                break;
-                            case "r":
-                            case "right":
-                                dir = directionType.right;
-                                break;
-                            case "u":
-                            case "up":
-                                dir = directionType.up;
-                                break;
-                            case "d":
-                            case "down":
-                                dir = directionType.down;
-                                break;
-                            default:
-                                Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongFirstText());
+                            if (comma < 0)
+                            {
+                                Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongNumerText());
                                 return;
+                            }
+
+                            string param1 = usedCommandsList[i].Substring(bracket + 1, comma - (bracket + 1)).Replace(" ", string.Empty);
+                            directionType dir;
+
+                            //if (param1==directionType.down.ToString() || param1 == directionType.up.ToString() || param1 == directionType.left.ToString() || param1 == directionType.right.ToString())
+                            //    dir = (directionType)Enum.Parse(typeof(directionType), param1);
+                            //else
+                            //{
+                            //    Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongFirstText());
+                            //    return;
+                            //}
+
+                            switch (param1)
+                            {
+                                case "l":
+                                case "left":
+                                    dir = directionType.left;
+                                    break;
+                                case "r":
+                                case "right":
+                                    dir = directionType.right;
+                                    break;
+                                case "u":
+                                case "up":
+                                    dir = directionType.up;
+                                    break;
+                                case "d":
+                                case "down":
+                                    dir = directionType.down;
+                                    break;
+                                default:
+                                    Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongFirstText());
+                                    return;
+                            }
+
+
+
+                            //check number
+                            int iterations;
+                            int.TryParse(usedCommandsList[i].Substring(comma + 1, usedCommandsList[i].IndexOf(')') - (comma + 1)).Replace(" ", string.Empty), out iterations);
+                            if (iterations == 0)
+                            {
+                                Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongSecondText());
+                                return;
+                            }
+
+                            MoveCommand move = new MoveCommand(actor, i, dir, iterations);
+                            commands.Add(move);
+                            break;
                         }
-
-                       
-
-                        //check number
-                        int iterations;
-                        int.TryParse(usedCommandsList[i].Substring(comma + 1, usedCommandsList[i].IndexOf(')') - (comma + 1)).Replace(" ", string.Empty), out iterations);
-                        if (iterations == 0)
+                    case "use":
                         {
-                            Console2.Instance.AddFeedback(i, TextManager.Instance.GetWrongSecondText());
-                            return;
-                        }  
-                        
-                        MoveCommand move = new MoveCommand(actor, i, dir, iterations);
-                        commands.Add(move);
-                        break;
-                    }
-                case "use":
-                    {
-                        InteractCommand move = new InteractCommand(actor, i);
-                        commands.Add(move);
-                        break;
-                    }
-                case "pos":
-                    {
-                        PossessCommand move = new PossessCommand(actor, i);
-                        commands.Add(move);
-                        break;
-                    }
-                case "winner":
-                    {
-                        GameManager.Instance.CompleteLevel();
-                        break;
-                    }
-            }
+                            InteractCommand move = new InteractCommand(actor, i);
+                            commands.Add(move);
+                            break;
+                        }
+                    case "pos":
+                        {
+                            PossessCommand move = new PossessCommand(actor, i);
+                            commands.Add(move);
+                            break;
+                        }
+                    case "winner":
+                        {
+                            GameManager.Instance.CompleteLevel();
+                            break;
+                        }
+                }
 
-            if (usedCommandsList[i].IndexOf(");") != usedCommandsList[i].Length - 2)
-            {
-                Console2.Instance.AddFeedback(i, TextManager.Instance.GetSemicolonText());
-                return;
+                if (usedCommandsList[i].IndexOf(");") != usedCommandsList[i].Length - 2)
+                {
+                    Console2.Instance.AddFeedback(i, TextManager.Instance.GetSemicolonText());
+                    return;
+                }
             }
         }
-
 
         usedCommandsList.Clear();
 
